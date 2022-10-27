afef49848950714b88b82c8eca8c12db80731b2e
Đường đen <79037079+duongden@users.noreply.github.com>
2022-10-27 12:53:58 +0700

    add 2cyxsw.com

---
+++ 2cyxsw/src/detail.js
@@ -0,0 +1,22 @@
+function execute(url) {
+    url = url.replace('m.2cyxsw.com', 'www.2cyxsw.com');
+    let response = fetch(url);
+    if (response.ok) {
+
+        let doc = response.html();
+        let coverImg = doc.select(".imgbox img").first().attr("src");
+        if (coverImg.startsWith("/")) {
+            coverImg = "http://www.2cyxsw.com" + coverImg;
+        }
+        let author = doc.select(".info p").first().text().replace(/作\s*者：/g, "");
+        return Response.success({
+            name: doc.select(".info h1").text(),
+            cover: coverImg,
+            author: author,
+            description: doc.select(".desc").text(),
+            detail: "作者：" + author + "<br>" +doc.select(".fix p").get(4).html(),
+            host: "http://www.2cyxsw.com"
+        });
+    }
+    return null;
+}
---
+++ 2cyxsw/src/home.js
@@ -0,0 +1,6 @@
+function execute() {
+    return Response.success([
+        {title: "首页", input: "https://www.2cyxsw.com/", script: "gen.js"}
+
+    ]);
+}
