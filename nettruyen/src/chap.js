6a98a8ebc7aaeba292b89faaf45af221f66e87ce
Đường đen <79037079+duongden@users.noreply.github.com>
2022-12-13 10:15:21 +0700

    cập nhật tên miền nettruyen

---
+++ nettruyen/src/chap.js
@@ -0,0 +1,23 @@
+function execute(url) {
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img,"https://www.nettruyenking.com/")
+
+    var doc = Http.get(url).html();
+    var el = doc.select(".page-chapter img");
+    
+    var data = [];
+    for (var i = 0; i < el.size(); i++) {
+        var e = el.get(i);
+        var img = e.attr("data-original");
+        if (!img) {
+            img = e.attr("src");
+        }
+
+        if (img) {
+            if (img.startsWith("//")) {
+                img = "http:" + img;
+            }
+            data.push(img);
+        }
+    }
+    return Response.success(data);
+}
---
+++ nettruyen/src/detail.js
@@ -0,0 +1,18 @@
+function execute(url) {
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img,"https://www.nettruyenking.com/")
+    
+    const doc = Http.get(url).html()
+    var coverImg = doc.select(".detail-info img").first().attr("src");
+    if (coverImg.startsWith("//")) {
+        coverImg = "https:" + coverImg
+    }
+    return Response.success({
+        name: doc.select("h1.title-detail").first().text(),
+        cover: coverImg,
+        author: doc.select(".author a").first().text(),
+        description: doc.select(".detail-content p").html(),
+        detail: doc.select(".list-info").html(),
+        host: "https://www.nettruyenking.com",
+        ongoing: doc.select(".detail-info .status").html().indexOf("Đang tiến hành") >= 0
+    });
+}
