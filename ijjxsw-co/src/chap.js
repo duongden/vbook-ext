b361abd6f1e6a471720f4f8c0eaa994b11d37c67
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-01-15 22:35:53 +0700

    fix ijjxsw.co

--- ijjxsw-co/src/chap.js
+++ ijjxsw-co/src/chap.js
@@ -1,12 +1,22 @@
 function execute(url) {
     url = url.replace('m.ijjxsw.co', 'www.ijjxsw.co');
-    let response = fetch(url);
-
-    if (response.ok) {
-        let doc = response.html();
-        let htm = doc.select("#view_content_txt").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+    let cvData ="";
+    let part1 = url.replace("http://www.ijjxsw.co", "").replace("https://www.ijjxsw.co", "").replace(".html","");
+    var next = part1;
+    while (next.includes(part1)) {
+        let response = fetch("http://www.ijjxsw.co" + next +".html");
+        if (response.ok) {
+            let doc = response.html();
+            next = doc.select("a#next_url").attr("href").replace(".html","");
+            let htm = doc.select("#view_content_txt").html();
+            htm = htm.replace(/\&nbsp;/g, "");
+            cvData = cvData + htm;
+        } else {
+            break;
+        }
+    }
+    if (cvData) {
+        return Response.success(cvData);
     }
     return null;
 }
