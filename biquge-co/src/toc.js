6011e95b7040969a22c47c5c1503c3fba879ac44
Đường đen <79037079+duongden@users.noreply.github.com>
2023-03-12 16:54:08 +0700

    update toc.js

--- biquge-co/src/toc.js
+++ biquge-co/src/toc.js
@@ -1,3 +1,4 @@
+
 load('libs.js');
 
 function execute(url) {
@@ -6,16 +7,22 @@ function execute(url) {
     var doc = Http.get(url).html('gbk');
 
     var data = [];
-    var elems = $.QA(doc, '#list dd a');
+    var elems = $.QA(doc, '#list a');
     if (!elems.length) return Response.error(url);
-    
-    elems.forEach(function(e) {
+
+    if (elems.length >= 12 * 2) {
+        elems.splice(0, 9); // Remove first 12 items
+    } else {
+        elems.splice(0, elems.length / 2);
+    }
+
+    elems.forEach(function (e) {
         data.push({
-            name: e.text(),
-            url: e.attr('href'),
-            host: host,
+            name: $.Q(e, 'a').text(),
+            url: e.attr('href').mayBeFillHost(url), // Important
+            host: host
         })
-    });
+    })
 
     return Response.success(data);
 }
