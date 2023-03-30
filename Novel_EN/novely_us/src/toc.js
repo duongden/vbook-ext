ddd514707418ec2a46a88f8c976e56ac42e95dab
Đường đen <79037079+duongden@users.noreply.github.com>
2023-03-30 11:06:04 +0700

    add novely.us

---
+++ Novel_EN/novely_us/src/toc.js
@@ -0,0 +1,18 @@
+
+function execute(url) {
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html();
+        doc.select("#tab-b0").remove();
+//#tab-b1 > div.chapters
+        let list = [];
+        doc.select("#tab-b1 div.chapters ul li a").forEach(e => list.push({
+                name: e.select("a span").text(),
+                url: e.attr("href").trim(),
+                host: "https://novely.us/"
+        }));
+        return Response.success(list);
+
+    }
+    return null;
+}
