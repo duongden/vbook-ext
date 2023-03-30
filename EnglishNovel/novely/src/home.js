3a538c944e4ce532a01cbfeda2278ad068cbfa5a
Đường đen <79037079+duongden@users.noreply.github.com>
2023-03-30 12:52:52 +0700

    .

---
+++ EnglishNovel/novely/src/home.js
@@ -0,0 +1,27 @@
+function execute() {
+    return Response.success([
+        { title: "Web Novel", input: "https://novely.us/web-novel", script: "gen.js" },
+        { title: "Light Novel", input: "https://novely.us/light-novel", script: "gen.js" },
+        { title: "Fantasy", input: "https://novely.us/genre/fantasy", script: "gen.js" },
+        { title: "Romance", input: "https://novely.us/genre/romance", script: "gen.js" },
+        { title: "Action", input: "https://novely.us/genre/action", script: "gen.js" },
+        { title: "Adventure", input: "https://novely.us/genre/adventure", script: "gen.js" },
+        { title: "Comedy", input: "https://novely.us/genre/comedy", script: "gen.js" },
+        { title: "Updates", input: "https://novely.us/genre/martial-arts", script: "gen.js" },
+        { title: "Martial Arts", input: "https://novely.us/web-novel", script: "gen.js" },
+        { title: "Drama", input: "https://novely.us/genre/drama", script: "gen.js" },
+        { title: "Harem", input: "https://novely.us/genre/harem", script: "gen.js" },
+        { title: "Supernatural", input: "https://novely.us/genre/supernatural", script: "gen.js" },
+        { title: "Slice of Life", input: "https://novely.us/genre/slice-of-life", script: "gen.js" },
+        { title: "Xianxia", input: "https://novely.us/genre/xianxia", script: "gen.js" },
+        { title: "Mature", input: "https://novely.us/genre/mature", script: "gen.js" },
+        { title: "Sci-fi", input: "https://novely.us/genre/sci-fi", script: "gen.js" },
+        { title: "Josei", input: "https://novely.us/genre/josei", script: "gen.js" },
+        { title: "School Life", input: "https://novely.us/genre/school-life", script: "gen.js" },
+        { title: "Historical", input: "https://novely.us/genre/historical", script: "gen.js" },
+        { title: "Seinen", input: "https://novely.us/genre/seinen", script: "gen.js" },
+        { title: "Xuanhuan", input: "https://novely.us/genre/xuanhuan", script: "gen.js" },
+        { title: "Mystery", input: "https://novely.us/genre/mystery", script: "gen.js" },
+        { title: "Shounen", input: "https://novely.us/genre/shounen", script: "gen.js" }                        
+    ])
+}
