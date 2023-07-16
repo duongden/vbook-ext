5137896654a8e8bf8af9215f86becca32948e57d
Đường đen <79037079+duongden@users.noreply.github.com>
2023-07-16 15:42:58 +0700

    update

--- Novel_Zh/112738/src/search.js
+++ Novel_Zh/112738/src/search.js
@@ -1,27 +1,18 @@
 load('config.js');
-function execute(key, page) {
-    let response = fetch(BASE_URL + '/searchb6.html', {
-        method: "GET",
-        queries: {
-            keyword: key,
-            //mysearch
-        }
-    });
+function execute(key) {
+    let response = fetch(BASE_URL + '/search2c.html?searchkey=' + key);
 
     if (response.ok) {
         let doc = response.html();
         const data = [];
-
-        doc.select(".item").forEach(e => {
+		doc.select(".item").forEach(e => {
             data.push({
                 name: e.select("dl dt a").first().text(),
-                link: e.select("dl dt a").first().attr("href"),
-                cover: e.select(".image a img").attr("data-original"),
+                link: BASE_URL + e.select("dl dt a").first().attr("href"),
                 description: e.select(".btm").first().text().replace(/\//g, "").trim(),
                 host: BASE_URL
             })
         });
-
         return Response.success(data);
     }
     return null;
