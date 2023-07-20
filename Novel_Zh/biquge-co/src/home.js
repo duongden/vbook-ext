80371ad91d03898c898f5548bed8ff4355568324
Đường đen <79037079+duongden@users.noreply.github.com>
2023-07-20 14:02:07 +0700

    .

--- Novel_Zh/biquge-co/src/home.js
+++ Novel_Zh/biquge-co/src/home.js
@@ -1,6 +1,5 @@
 function execute() {
     return Response.success([
-        {title: "首页", input: "/", script: "gen.js"},
         {title: "玄幻小说", input: "/xuanhuanxiaoshuo/1_{0}.html", script: "gen.js"},
         {title: "修真小说", input: "/xiuzhenxiaoshuo/2_{0}.html", script: "gen.js"},
         {title: "都市小说", input: "/dushixiaoshuo/3_{0}.html", script: "gen.js"},
