f5242d3a7a2e7255928af2bd9180627f64a9bd24
Đường đen <79037079+duongden@users.noreply.github.com>
2023-09-19 14:58:37 +0700

    .

--- Comic_EN/zinmanga/src/search.js
+++ Comic_EN/zinmanga/src/search.js
@@ -1,28 +1,22 @@
 function execute(key, page) {
-
-    let response = fetch("https://readmanga.org/search/autocomplete", {
-        method: "GET",
-        queries: {
-            dataType: "json",
-            query: key
-        }
-    });
+    if (!page) page = 1;
+    let response = fetch('https://zinmanga.com/?s=' + key + "&post_type=wp-manga");
 
     if (response.ok) {
-        let json = response.json();
+        let doc = response.html();
         const data = [];
-        json.results.forEach(e => {
+        console.log(doc)
+		doc.select(".c-tabs-item__content").forEach(e => {
             data.push({
-                name: e.original_title,
-                link: e.link,
-                cover: e.image,
-                description: e.overview,
-                host: "https://readmanga.org"
+                name: e.select(".h4 a").first().text(),
+                link: e.select(".h4 a").first().attr("href"),
+                cover: e.select(".c-image-hover img").first().attr("data-src"),
+                description: e.select(".total_votes").first().text(),
+                host: "https://zinmanga.com"
             })
         });
-
-        return Response.success(data)
+        var next = (parseInt(page)+1).toString();
+        return Response.success(data,next)
     }
-
     return null;
 }
