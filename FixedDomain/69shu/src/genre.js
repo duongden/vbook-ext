19405689faf0a9a1894f1d2b3850618ef6d1672c
Đường đen <79037079+duongden@users.noreply.github.com>
2024-03-01 06:27:29 +0700

    Add files via upload

--- FixedDomain/69shu/src/genre.js
+++ FixedDomain/69shu/src/genre.js
@@ -1,7 +1,7 @@
 load('config.js');
 function execute() {
-
-    let response = fetch(BASE_URL + "/newtags");
+//https://www.69shu.pro/newtag/%E8%BD%BB%E6%9D%BE/
+    let response = fetch(BASE_URL + "/newtag/");
     if (response.ok) {
         let doc = response.html('gbk');
         let menu = doc.select(".tag ul a")
