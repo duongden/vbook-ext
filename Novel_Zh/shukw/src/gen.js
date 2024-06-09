c5e7811d9ec32e358e624e7070bd625a9a7cfc8b
Đường đen <79037079+duongden@users.noreply.github.com>
2024-06-09 13:36:36 +0700

    fix bug

--- Novel_Zh/shukw/src/gen.js
+++ Novel_Zh/shukw/src/gen.js
@@ -1,22 +1,20 @@
 load('config.js');
 function execute(url) {
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+    if(url.slice(-1) === "/")
+        url = url.slice(0, -1)
     let response = fetch(BASE_URL + url);
+    console.log(BASE_URL + url)
     if (response.ok) {
         let doc = response.html();
         const data = [];
-		doc.select("#newscontent .l li").forEach(e => {
-            let coverImg = "https://raw.githubusercontent.com/duongden/vbook/main/nocover.jpg";
+        doc.select(".l li").forEach(e => {
             data.push({
                 name: e.select(".s2 a").first().text(),
                 link: BASE_URL + e.select(".s2 a").first().attr("href"),
-                description: e.select(".s3 a").first().text(),
-                cover: coverImg,
+                description: e.select(".s3").first().text(),
                 host: BASE_URL
             })
         });
-
-
         return Response.success(data)
     }
     return null;
